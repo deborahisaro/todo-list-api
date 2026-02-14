3a543a29b51881b8e23e7f2bd11e7dfc1645acba
Deborah <isarodeborah85@gmail.com>
2026-02-14 21:43:29 +0200

    heheheeh

--- index.js
+++ index.js
@@ -113,7 +113,6 @@ app.delete("/tasks/:id";(req,res)=>{
   res.send("task deleted");
 });
 console.log("Server is running on http://localhost:3000");
-console.log("Server is running on http://localhost:3000");
 
 
 
